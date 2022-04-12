a7dfde9dddcf44d2ae233390715c29b545575181
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-04-12 16:19:11 -0300

    Js files and vscode settings

---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "345670;"
+
+const citacao = "Meu nome é ";
+const meuNome = "Danilo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+
+
+// template string ou template literal
+
+const templateString = `template`;
