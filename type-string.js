c1d0a7aafd5043abbf4560a02f588b74d6a1400e
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-04-12 17:24:41 -0300

    replacing all files

--- type-string.js
+++ type-string.js
@@ -1,17 +1,13 @@
 const texto1 = "Olá, mundo!";
 const texto2 = 'Olá, mundo!';
 const senha = "senhaSuperSegura456!";
-const StringDeNumeros = "345670;"
+const StringDeNumeros = "34567";
 
 const citacao = "Meu nome é ";
-const meuNome = "Danilo";
+const meuNome = "Leonardo";
 
 // concatenação (+)
 
-console.log(citacao + meuNome);
+console.log(citacao + meuNome)
 
-
-
-// template string ou template literal
-
-const templateString = `template`;
+//template string OU template literal
