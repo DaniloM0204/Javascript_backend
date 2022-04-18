2b084b2002a0f9ec870455abb4b1d8c1d155ae46
DaniloM0204 <danilo_mbezerra@hotmail.com>
2022-04-18 13:33:18 -0300

    Commit arrays.js

---
+++ desafio_nota.js
@@ -0,0 +1,19 @@
+//Parte 1
+
+// const notas = [10, 6, 8];
+// console.log(notas);
+
+// notas.push(7)
+// console.log(notas);
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3])/ notas.length;
+// console.log(media);
+
+// Parte 2
+
+const notas = [10, 6, 8, 5.5, 10]
+notas.pop();
+console.log(notas);
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3])/ notas.length;
+console.log(`A media é ${media}`);
